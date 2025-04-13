e18d42031f2cd345f78b12c3c21d9c9bac01788d
Shane Wade <shanebramble96@hotmail.com>
2025-04-13 18:48:18 -0500

    created functional server

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import path from 'path';
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+/**
+ * Express middleware configuration
+ */
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files from 'public' directory
+
 
 app.listen(PORT, () => {
   console.log("App is listening..");
