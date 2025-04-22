2886b6049968c6fdfa9e09597abcdaf9d27e30e8
Shane Wade <shanebramble96@hotmail.com>
2025-04-21 19:01:44 -0500

    created a seed script file

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { sequelize } from "./config/connections.js";
-import { User, Post, Comment } from "./models/index.js";
+// import { User, Post, Comment } from "./models/index.js";
+import controller from "./controller/index.js";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
 
@@ -15,6 +16,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from 'public' directory
+
+// Use the api routes
+app.use(controller);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
