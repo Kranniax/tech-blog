813db35d4234f31444445a466442c098edcf3964
Shane Wade <shanebramble96@hotmail.com>
2025-04-30 20:02:01 -0500

    troubleshooting dashboard view route

--- controller/dashboard-routes.js
+++ controller/dashboard-routes.js
@@ -4,15 +4,27 @@ import { User, Post } from "../models/index.js";
 const router = Router();
 // get the posts for a single user
 router.get("/:id", (req, res) => {
-  User.findAll({
+  Post.findAll({
     where: {
-      id: req.params.id,
+      user_id: req.params.id,
     },
+    include: [
+      {
+        model: User,
+        attributes: ["id", "username"],
+      },
+    ],
   }).then((dbUserData) => {
     if (!dbUserData) {
       res.status(404).json({ message: "Posts cannot be found from this user" });
       return;
     }
+    // serialize db data before rendering.
+    const userData = dbUserData.map((user) => user.get({ plain: true }));
+    // res.json(dbUserData);
+
+    // render data to dashboard handlebars template.
+    res.render("dashboard", { userData });
   });
 });
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { sequelize } from "./config/connections.js";
 import { engine } from "express-handlebars";
+import session from "express-session";
 import controller from "./controller/index.js";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
@@ -17,7 +18,25 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 app.use(express.static(path.join(__dirname, "public"))); // Serve static files from 'public' directory
 
-// register a handlebars view engine 
+// Configure session
+const sess = {
+  secret: "^6>2geVzOC=?C=:NVm?C", // Use environment variable in production
+  cookie: {
+    maxAge: 1800000, // 30 minutes
+    httpOnly: true,
+    secure: false,
+    sameSite: "strict",
+  },
+  resave: false,
+  saveUninitialized: true,
+};
+
+
+// set up sessions in express.
+app.use(session(sess));
+
+
+// register a handlebars view engine
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
