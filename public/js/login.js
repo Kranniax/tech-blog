0581409aa3006d10e9949af3c1242eceeffc05ba
Shane Wade <shanebramble96@hotmail.com>
2025-05-04 19:22:04 -0500

    login authentication operational

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,28 @@
-var signupFormHandler = (event) => {
+var loginFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
-  if (username && email && password) {
-    fetch("/api/users", {
+  if (email && password) {
+    const response = await fetch("/api/users/login", {
       method: "post",
       body: JSON.stringify({
-        username,
         email,
         password,
       }),
       headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      console.log(response);
     });
+
+    // check the respone status
+    if (response.ok) {
+        // if sucessful render the homepage.  
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
   }
 };
 
 document
-  .querySelector("#signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
